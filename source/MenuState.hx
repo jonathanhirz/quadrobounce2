8b3465a092025fb66d8d002edef176313f7d7ba9
Jonathan Hirz <jonathanhirz@gmail.com>
2014-04-05 08:20:28 -0700

    back to other color

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -19,10 +19,10 @@ class MenuState extends FlxState
 	{
 		super.create();
 
-		FlxG.camera.bgColor = 0xff8b1cd9;
+		FlxG.camera.bgColor = 0xffd91414;
 
 		add(new FlxText(30,FlxG.height/2 - 140,640,"quadrobounce2",60));
-		add(new FlxText(40,FlxG.height/2,640,"SPACE to start dude",20));
+		add(new FlxText(40,FlxG.height/2,640,"SPACE to start",20));
 		add(new FlxText(40,FlxG.height/2+30,640,"ARROWS or WASD to move",20));
 		add(new FlxText(40,FlxG.height/2+60,640,"Keep the ball on the screen",20));
 	}
